2e2e5fdef76c01e2ce04fc102a13f1328d612232
Ana Lopes <anapedra.mil@gmail.com>
2022-11-27 22:01:52 -0300

    Backend project created

---
+++ backend/dsuser/src/main/java/com/anapazsatanas/dsuser/DsuserApplication.java
@@ -0,0 +1,13 @@
+package com.anapazsatanas.dsuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsuserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DsuserApplication.class, args);
+    }
+
+}
---
+++ backend/dsuser/src/test/java/com/anapazsatanas/dsuser/DsuserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anapazsatanas.dsuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsuserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
